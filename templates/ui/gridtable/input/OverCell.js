c973b6efaccf9d10cb786fc17230223d2860e020
Rex <binhua.tein@gmail.com>
2026-02-24 09:07:43 +0800

    Reverse logic

--- templates/ui/gridtable/input/OverCell.js
+++ templates/ui/gridtable/input/OverCell.js
@@ -1,4 +1,4 @@
-import PointerOverTest from './PointerOverTest.js';
+import EmitCellEvent from './EmitCellEvent.js';
 
 var OverCell = function (table, tableConfig) {
     table
@@ -8,11 +8,27 @@ var OverCell = function (table, tableConfig) {
 }
 
 var OnMove = function (pointer, localX, localY, event) {
-    this.pointerOverTest(pointer, event);
+    var table = this.childrenMap.child;
+    var camera = pointer.camera;
+    var px = pointer.worldX + camera.scrollX * (table.scrollFactorX - 1);
+    var py = pointer.worldY + camera.scrollY * (table.scrollFactorY - 1);
+
+    var cellIndex = table.pointToCellIndex(px, py);
+    if (cellIndex === table.input.lastOverCellIndex) {
+        return;
+    }
+
+    var preCellIndex = table.input.lastOverCellIndex;
+    table.input.lastOverCellIndex = cellIndex;
+    EmitCellEvent(this.eventEmitter, 'cell.out', table, preCellIndex, undefined, pointer, event);
+    EmitCellEvent(this.eventEmitter, 'cell.over', table, cellIndex, undefined, pointer, event);
 }
 
 var OnOut = function (pointer, event) {
-    this.pointerOverTest(pointer, event);
+    var table = this.childrenMap.child;
+    var cellIndex = table.input.lastOverCellIndex;
+    table.input.lastOverCellIndex = undefined;
+    EmitCellEvent(this.eventEmitter, 'cell.out', table, cellIndex, undefined, pointer, event);
 }
 
 export default OverCell;
